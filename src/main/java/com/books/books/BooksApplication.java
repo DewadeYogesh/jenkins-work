ce1edf6dd735ff9dde585926a339930b76199ad1
Yogesh <yogeshdewade860@gmail.com>
2023-08-09 17:27:03 +0530

    Second commit

--- src/main/java/com/books/books/BooksApplication.java
+++ src/main/java/com/books/books/BooksApplication.java
@@ -1,12 +1,16 @@
 package com.books.books;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BooksApplication {
+	public static Logger logger= LoggerFactory.getLogger(BooksApplication.class);
 
 	public static void main(String[] args) {
+		logger.info("Program Started");
 		SpringApplication.run(BooksApplication.class, args);
 	}
 
